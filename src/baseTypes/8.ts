9662dc6859f0c45c232e863940aa3eae711a0c75
AllaGrebennik <grebennik.alla@gmail.com>
2024-04-09 20:19:35 +0300

    ts-hw-2

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -2,7 +2,7 @@
   Створіть тип "Gender", використовуючи union type, 
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
-
-const myGender;
+type myGender = "male" | "female";
+const myGender = "male";
 
 export {};
